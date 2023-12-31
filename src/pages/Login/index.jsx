30a2eb528a2d65032e9ffec229083426ff5ee641
DouZhanShengYang <3555936530@qq.com>
2023-12-31 16:48:08 +0800

    搭建基本环境成功

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,6 @@
+import {Button} from 'antd'
+
+const Login=()=>{
+    return <div>this is Login<Button type='primary'>test</Button></div>
+}
+export default Login
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from "@/pages/Login";
+import Layout from "@/pages/Layout";
+
+const router=createBrowserRouter([
+    {
+        path:'/',
+        element: <Layout></Layout>,
+    },
+    {
+        path:'/login',
+        element: <Login></Login>,
+    },
+])
+export default router
